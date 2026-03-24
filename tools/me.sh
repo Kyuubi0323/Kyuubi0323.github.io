35bd5a50b28ac2feaf27f0fcf8977bfa67178993
Kyuubi0323 <khoi.nv202647@sis.hust.edu.vn>
2026-03-25 00:42:23 +0700

    _add docs about tools

--- tools/me.sh
+++ tools/me.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-#
+# this scripts aim to acess directly this localhost
+# of VM in our windows host
 # Run jekyll serve and then launch the site
 bundle exec jekyll serve --host 0.0.0.0 --port 4000
